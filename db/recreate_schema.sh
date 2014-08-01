60b30335db5c76305de71da2979d18ec368fce44
cmr <dev@cmr.cx>
2014-08-01 14:48:26 +0200

    use config.php in bash scripts

--- db/recreate_schema.sh
+++ db/recreate_schema.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# delete all database content and recreate the schema
 
-dbname=basisentscheid
-dbuser=basisentscheid
+dbname=$( php -r 'define("DOCROOT", "../"); require DOCROOT."inc/config.php"; preg_match("/dbname=(\w+)/", DATABASE_CONNECT, $matches); echo $matches[1];' )
+dbuser=$( php -r 'define("DOCROOT", "../"); require DOCROOT."inc/config.php"; preg_match("/user=(\w+)/",   DATABASE_CONNECT, $matches); echo $matches[1];' )
 
 echo "DROP SCHEMA public CASCADE; CREATE SCHEMA public;" | psql --username=$dbuser -v ON_ERROR_STOP=1 $dbname && \
 psql --username=$dbuser -q -f basisentscheid.sql $dbname
