e3dcbe3fb4fdb9b375f333ddc51f003c801bcd4e
cmr <dev@cmr.cx>
2014-08-11 17:01:30 +0200

    workaround for gettext caching

--- inc/locale.php
+++ inc/locale.php
@@ -22,6 +22,17 @@ $locale = $languages[LANG];
 if ( setlocale(LC_MESSAGES, $locale) === false ) {
 	trigger_error("setlocale to ".$locale." failed", E_USER_WARNING);
 }
-bindtextdomain("messages", "./locale");
-textdomain("messages");
-bind_textdomain_codeset("messages", "UTF-8");
+
+if (DEBUG) {
+	// workaround for gettext caching
+	$domains = glob("locale/".LANG."/LC_MESSAGES/messages-*.mo");
+	$current = basename($domains[0], ".mo");
+	bindtextdomain($current, "./locale");
+	textdomain($current);
+	bind_textdomain_codeset($current, "UTF-8");
+} else {
+	// In live environment probably the webserver has to be gracefully restarted to load a new gettext file.
+	bindtextdomain("messages", "./locale");
+	textdomain("messages");
+	bind_textdomain_codeset("messages", "UTF-8");
+}
