12936aff764409c9e4e36e1c8356144359b74028
cmr <dev@cmr.cx>
2015-01-01 23:31:13 +0100

    PHP configuration

--- inc/common_cli.php
+++ inc/common_cli.php
@@ -9,6 +9,10 @@
 
 require DOCROOT."inc/config.php";
 require DOCROOT."inc/errors.php";
+
+ini_set('default_charset', 'UTF-8');
+mb_internal_encoding("UTF-8");
+
 require DOCROOT."inc/common.php";
 require DOCROOT."inc/functions.php";
 require DOCROOT."inc/functions_cli.php";
