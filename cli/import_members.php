facd5212c2b9b50eae5a34e782483d55c4dded85
cmr <dev@cmr.cx>
2015-01-06 23:10:23 +0100

    import groups with hierarchy

--- cli/import_members.php
+++ cli/import_members.php
@@ -4,18 +4,19 @@
  * Import members from CSV file
  *
  * Not yet existing members will be created, existing members will be updated.
- * Having also otherwise created members is possible, but will lead to warnings.
+ * Having also otherwise created members is possible for testing, but will lead to warnings.
  * Existing groups with case-insensitive matching names will be used, missing groups will be created.
  * Groups may only be renamed simultaneously on both sides if not only casing is changed!
+ * If groups change their parent group, warnings will be shown until the parent assignment is updated in the database manually.
  *
  * Format of the CSV file:
- * Line: <invite_code>;<eligible>;<verified>;<group_name>;<group_name>;<group_name>...
- * Last line: EOF;<number_of_lines_without_the_eof_line>
+ *   Line: <invite_code>;<eligible>;<verified>;<group_level1_name>;<group_level2_name>;<group_level3_name>...
+ *   Last line: EOF;<number_of_lines_without_the_eof_line>
  * The invite code must consist of exactly 24 characters, preferably only out of [A-Za-z0-9].
  * Values for <eligible> and <verified> are 1 for true and 0 for false.
- * The groups are optional, having no groups is fine.
+ * Groups have to be listed in hierarchical order from top to subgroups. Any number of groups is allowed, even no groups at all.
  * Fields may be enclosed in double quotes ("), but this is only required if a field contains the column delimiter (;).
- * No delimiter is allowed at the end of the line.
+ * No delimiter is allowed at the end each line.
  * Charset: UTF-8
  * Line breaks: Unix (LF)
  *
@@ -36,8 +37,9 @@ require "../inc/common_cli.php";
 $before = DB::fetchfield("SELECT COUNT(*) FROM member");
 
 $inserted = 0;
-$lines = 0;
+$line = 0;
 $eof = false;
+$ngroup_map = array();
 
 $handle = fopen($_SERVER['argv'][1], "r");
 if (!$handle) exit(1);
@@ -49,27 +51,49 @@ while ( ($data = fgetcsv($handle, 256, ";")) !== false ) {
 		$eof = true;
 		// check number of lines
 		$eof_lines = $data[1];
-		if ($eof_lines != $lines) {
-			echo "WARNING: The number of lines in the CSV file ($lines) is not equal to the number of lines stated in the last line ($eof_lines)!\n";
+		if ($eof_lines != $line) {
+			echo "WARNING: The number of lines in the CSV file ($line) is not equal to the number of lines stated in the last line ($eof_lines)!\n";
 		}
 		break;
 	}
 
-	$lines++;
+	$line++;
 
 	// extract invite code
 	$invite = $data[0];
 	if (!$invite) {
-		echo "WARNING: No invite code could be extracted from this line: $line\n";
+		echo "WARNING: No invite code could be extracted from line $line!\n";
 		continue;
 	}
 
-	// extract groups
+	// extract groups and convert convert them to ids
 	$ngroups = [];
+	$parent = null;
 	for ( $index = 3; $index < count($data); $index++ ) {
 		$name = trim($data[$index]);
 		if (!$name) continue; // skip empty group names
-		$ngroups[] = ngroup_by_name($name);
+		if (!isset($ngroup_map[$name])) {
+			// get matching group
+			$result = DB::query("SELECT * FROM ngroup WHERE name ILIKE ".DB::esc($name));
+			if ( $ngroup = DB::fetch_object($result, "Ngroup") ) {
+				if ( DB::num_rows($result) > 1 ) {
+					echo "WARNING: More than one matching groups for '$name' were found!\n";
+				}
+				if ($ngroup->parent != $parent) {
+					echo "WARNING: The parent of group '$name' is '$ngroup->parent' in the database, but '$parent' in line $line!\n";
+				}
+			} else {
+				// create group that does not yet exist
+				$ngroup = new Ngroup;
+				$ngroup->name   = $name;
+				$ngroup->parent = $parent;
+				$ngroup->create(['name', 'parent']);
+				echo "Created group '$name'\n";
+			}
+			$ngroup_map[$name] = $ngroup->id;
+		}
+		$ngroups[] = $ngroup_map[$name];
+		$parent    = $ngroup_map[$name];
 	}
 
 	$sql = "SELECT * FROM member WHERE invite=".DB::esc($invite);
@@ -102,8 +126,8 @@ if (!$eof) {
 $after = DB::fetchfield("SELECT COUNT(*) FROM member");
 
 // additional warnings
-if ($after != $lines) {
-	echo "WARNING: The number of members ($after) is not equal to the number of lines in the CSV file ($lines)!\n";
+if ($after != $line) {
+	echo "WARNING: The number of members ($after) is not equal to the number of lines in the CSV file ($line)!\n";
 }
 if ($after != ($before + $inserted) ) {
 	echo "WARNING: The number of members ($after) is not equal to the number before ($before) plus the inserted ($inserted) members!\n";
@@ -112,29 +136,5 @@ if ($after != ($before + $inserted) ) {
 // report
 echo "Members before:    $before\n";
 echo "Inserted members:  $inserted\n";
-echo "Lines in CSV file: $lines\n";
+echo "Lines in CSV file: $line\n";
 echo "Members:           $after\n";
-
-
-/**
- * translate a group name to an id
- *
- * @param string  $name
- * @return integer
- */
-function ngroup_by_name($name) {
-	static $ngroup_map = [];
-	if (!isset($ngroup_map[$name])) {
-		$id = DB::fetchfield("SELECT id FROM ngroup WHERE name ILIKE ".DB::esc($name));
-		if ($id) {
-			$ngroup_map[$name] = $id;
-		} else {
-			// group does not yet exist
-			$ngroup = new Ngroup;
-			$ngroup->name = $name;
-			$ngroup->create();
-			$ngroup_map[$name] = $ngroup->id;
-		}
-	}
-	return $ngroup_map[$name];
-}
--- cli/test_import_members.php
+++ cli/test_import_members.php
@@ -17,7 +17,7 @@ require "../inc/common_cli.php";
 $data = <<<EOT
 TESTieweedeeXailahzupa8v;1;1;Import group 1;Import group 2
 TESTMeevuighohda4ungai0d;1;0;Import group 1
-TESTed1cu5ohNg3majahY0qu;0;1;Import group 2
+TESTed1cu5ohNg3majahY0qu;0;1;Import group 1;Import group 3
 TESToThenedaiwiek0aiRi2A;0;0
 TESTcilie3quiemi5vo9Iw4H;1;1;Import group 1;Import group 2
 EOF;5
@@ -28,11 +28,11 @@ system("./import_members.php import_members_test.csv");
 // second import with updates
 $data = <<<EOT
 TESTieweedeeXailahzupa8v;0;0;Import group 1;Import group 2
-TESTMeevuighohda4ungai0d;0;1;Import group 1
-TESTed1cu5ohNg3majahY0qu;1;0;Import group 3
+TESTMeevuighohda4ungai0d;0;1;Import group 1;Import group 2
+TESTed1cu5ohNg3majahY0qu;1;0;Import group 1
 TESToThenedaiwiek0aiRi2A;0;0
 TESTphoo0poh3feYoo3uwoox;1;1
-TESThieroleJohgh8Die3oci;1;1;Import group 1;Import group 2;Import group 3
+TESThieroleJohgh8Die3oci;1;1;Import group 1;Import group 3
 EOF;6
 EOT;
 file_put_contents("import_members_test.csv", $data);
