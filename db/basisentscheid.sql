basisentscheid/basisentscheid

commit 7e57370d2a337be56d5b64dbe94e30edd77c4cd2

bug fixed: fogot to change the config-example period admin: save and make functional the input of vvvote delay interval and the time until the voting certificate can be generated

db/basisentscheid.sql
@@ -510,7 +510,10 @@ CREATE TABLE period (
     state period_state DEFAULT 'ballot_application'::period_state NOT NULL,
     postage boolean DEFAULT false NOT NULL,
     vvvote boolean DEFAULT false NOT NULL,
-    vvvote_configurl text
+    vvvote_configurl text,
+    vvvote_vote_delay TEXT,
+    vvvote_last_reg timestamp with time zone;
+    
 );
 
 

db/migrate.sql
@@ -160,3 +160,7 @@ CREATE UNIQUE INDEX vvvote_token_token_key ON vvvote_token USING BTREE (token);
 ALTER TABLE vvvote_token ADD generated BOOL DEFAULT FALSE NOT NULL;
 
 ALTER TABLE member ADD identity TEXT DEFAULT '' NOT NULL;
+
+ALTER TABLE period
+  ADD vvvote_vote_delay TEXT,
+  ADD vvvote_last_reg timestamp with time zone;

inc/classes/DbTableAdmin_Period.php
@@ -263,7 +263,42 @@ class DbTableAdmin_Period extends DbTableAdmin {
 				return false;
 			}
 		}
-
+		
+		
+		if ($this->object->vvvote) {
+			$vvvote_vote_delay = $this->object->vvvote_vote_delay;
+			$vvvote_last_reg = $this->object->vvvote_last_reg;
+			if (!$vvvote_last_reg) {
+				warning ( _ ( "The time until it is allowed to generate a voting certificate is requiered!" ) );
+				return false;
+			}
+			
+			$vvvote_last_reg = strtotime ( $vvvote_last_reg );
+			if ($vvvote_last_reg === false or $vvvote_last_reg <= 0) {
+				warning ( _("The time until it is allowed to generate a voting certificate is not valid!" ) );
+				return false;
+			}
+			$this->object->vvvote_last_reg = date ( "c", $vvvote_last_reg );
+			if ($vvvote_last_reg >= $counting) {
+				warning ( _ ("The time until it is allowed to generate a voting certificate must be before the counting starts!" ) );
+				return false;
+			}
+			
+			if ($vvvote_last_reg < $voting) {
+				warning ( _("The time until it is allowed to generate a voting certificate must be after the voting phase started!" ) );
+				return false;
+			}
+			
+			if (!$vvvote_vote_delay) {
+				warning ( _("The voting delay for online anonymous voting is required!") );
+				return false;
+				}
+			$time_tmp = strtotime ( "-" . $vvvote_vote_delay, $vvvote_last_reg );
+			if (! $time_tmp) {
+				warning ( _("The voting delay interval is not valid!" ) );
+				return false;
+			}
+		}
 		return true;
 	}
 	

inc/classes/Period.php
@@ -236,12 +236,12 @@ class Period extends Relation {
 		// calculate times
 		$time_voting   = strtotime($this->voting);
 		$time_counting = strtotime($this->counting);
-		$time_until = strtotime("-".VVVOTE_LAST_VOTING_INTERVAL, $time_counting);
+		$time_until = strtotime($this->vvvote_last_reg); // strtotime("-".VVVOTE_LAST_VOTING_INTERVAL, $time_counting);
 		$registration_end = $time_until;
 		$delay_until = array();
 		while ( $time_until > $time_voting ) {
 			$delay_until[] = gmdate("c", $time_until);
-			$time_until = strtotime("-".VVVOTE_VOTING_START_INTERVAL, $time_until);
+			$time_until = strtotime("-".$this->vvvote_vote_delay, $time_until);
 		}
 
 		$post = array(
@@ -659,6 +659,13 @@ class Period extends Relation {
 				break;
 			case "counting":
 				?> class="over"<?
+				break;
+			case "vvvote_last_reg":
+				if (strtotime($this->vvvote_last_reg) <= time()) {
+					?> class="over"<?
+				} else {
+					?> class="current"<?
+				}
 				break;
 			default:
 				trigger_error("invalid column name".$column[0], E_USER_NOTICE);

inc/config_example.php
@@ -51,6 +51,7 @@ const LANG = "en";
 const DATEYEAR_FORMAT     = "j.n.Y";
 const DATETIMEYEAR_FORMAT = "j.n.Y G:i";
 const DATE_FORMAT         = "j.n.";
+const DATETIMEYEAR_INPUT_FORMAT = "Y-m-d\TG:i";
 const DATETIME_FORMAT     = "j.n. G:i";
 const TIME_FORMAT         =      "G:i";
 const VOTETIME_FORMAT     = "d.m.Y H:i:s";
@@ -112,10 +113,6 @@ const VVVOTE_SERVERS = "";
 const VVVOTE_CONFIG_ID = "";
 // comma separated list of passwords to access vvvote_check_token.php
 const VVVOTE_CHECK_TOKEN_PASSWORDS = "";
-// time interval between voting start times
-const VVVOTE_VOTING_START_INTERVAL = "1 day";
-// time interval between last voting start time and voting end
-const VVVOTE_LAST_VOTING_INTERVAL = "1 hour";
 
 // --- Options for development and testing ---
 // display system informations in the HTML code of every page and disable gettext caching

periods.php
@@ -27,7 +27,7 @@ $d->columns = array(
 	array("postage",            _("Postage"),                     "center", "boolean",   "postage", 'type'=>"boolean"),
 	array(false,                _("Ballots"),                     "center", "ballots",   false),
 	array("vvvote_vote_delay",  _("Delay voting intervall"),      "period", "text", "interval", 'null'=>true), // each day
-	array("vvvote_last_reg",    _("Last moment to generate a voting certificate"),   "period", "timestamp", "timestamp", 'null'=>true) // 1 hour before counting
+	array("vvvote_last_reg",    _("Allow the generation of voting certificates until"),   "period", "timestamp", "timestamp", 'null'=>true) // 1 hour before counting
 );
 /*
 if (Login::$admin) {

commit acff200fe038dcb221def1f2d79a2c9c754e75d6

bug fixed: it was introduced by commit 7e57370d2a337be56d5b64dbe94e30edd77c4cd2 which added the functionallity: vvvote delay and the last date to create a voting certificate for each voting period

db/basisentscheid.sql
@@ -512,9 +512,8 @@ CREATE TABLE period (
     vvvote boolean DEFAULT false NOT NULL,
     vvvote_configurl text,
     vvvote_vote_delay TEXT,
-    vvvote_last_reg timestamp with time zone;
-    
-);
+    vvvote_last_reg timestamp with time zone
+ );
 
 
 --